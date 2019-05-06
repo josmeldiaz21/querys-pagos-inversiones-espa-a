b12832735a156a053a4d40404c065de8151b66fe
josmeldiaz21 <josmeldiaz21@gmail.com>
2019-05-06 16:38:23 -0400

    eliminacion de comentarios

--- 28.sql
+++ 28.sql
@@ -12,10 +12,9 @@ ON
 iep.id_desembolso = je.id_de_desembolso
 WHERE
 	iep.fecha_transaccion BETWEEN "2018-01-01" AND "2018-01-31" AND
-	#iep.metodo_credito = "Principal"
-	#AND
+
 	IF(iep.id_desembolso = je.id_de_desembolso,iep.metodo_credito = "Principal",iep.metodo_credito = "Comision")
-#AND iep.metodo_credito = "Comision"
+
 ORDER BY
 	iep.nombre_cliente,
 	iep.fecha_transaccion
