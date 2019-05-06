93190daf512f6c92636bc96bc61d3abe68ef1204
josmeldiaz21 <josmeldiaz21@gmail.com>
2019-05-06 10:37:12 -0400

    alias agregados

--- 112.sql
+++ 112.sql
@@ -1,28 +1,28 @@
 SELECT
 	tabCustomer.customer_name AS "Nombre:Data:150",
-	`tabINVERSIONES ESPANA PAGOS`.fecha_transaccion AS "Fecha",
-	`tabINVERSIONES ESPANA PAGOS`.id_desembolso AS "L19:Int:100",
-	`tabInversiones Espana Facturas`.ncf AS "Factura:Data:100",
-	`tabINVERSIONES ESPANA PAGOS`.monto_solicitado AS "Monto1:Data:150"
+	iep.fecha_transaccion AS "Fecha",
+	iep.id_desembolso AS "L19:Int:100",
+	ief.ncf AS "Factura:Data:100",
+	iep.monto_solicitado AS "Monto1:Data:150"
 FROM
-`tabINVERSIONES ESPANA PAGOS`
+`tabINVERSIONES ESPANA PAGOS` iep
 LEFT OUTER JOIN
 -- `tabSales Invoice`
-`tabInversiones Espana Facturas`
+`tabInversiones Espana Facturas` ief
 ON
 -- `tabINVERSIONES ESPANA PAGOS`.id_desembolso = `tabSales Invoice`.id_de_desembolso AND
-`tabINVERSIONES ESPANA PAGOS`.id_desembolso = `tabInversiones Espana Facturas`.id_desembolso AND
-`tabINVERSIONES ESPANA PAGOS`.monto_solicitado = `tabInversiones Espana Facturas`.monto_facturado
+iep.id_desembolso = ief.id_desembolso AND
+iep.monto_solicitado = ief.monto_facturado
 LEFT JOIN
 tabCustomer
 ON
-`tabINVERSIONES ESPANA PAGOS`.id_cliente = tabCustomer.id
+iep.id_cliente = tabCustomer.id
 WHERE
-`tabINVERSIONES ESPANA PAGOS`.fecha_transaccion >= "2018-01-01" AND
-`tabINVERSIONES ESPANA PAGOS`.fecha_transaccion <= "2018-01-31" AND
-`tabINVERSIONES ESPANA PAGOS`.metodo_credito != "Principal" AND
-`tabINVERSIONES ESPANA PAGOS`.metodo_credito != "Comision"
+iep.fecha_transaccion >= "2018-01-01" AND
+iep.fecha_transaccion <= "2018-01-31" AND
+iep.metodo_credito != "Principal" AND
+iep.metodo_credito != "Comision"
 ORDER BY
-`tabINVERSIONES ESPANA PAGOS`.nombre_cliente,
-`tabINVERSIONES ESPANA PAGOS`.fecha_transaccion,
-`tabInversiones Espana Facturas`.ncf
+iep.nombre_cliente,
+iep.fecha_transaccion,
+ief.ncf
