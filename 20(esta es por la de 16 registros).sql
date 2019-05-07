026907235822bb9e4fc30f0daa250ea6ad2c456a
josmeldiaz21 <josmeldiaz21@gmail.com>
2019-05-07 15:34:08 -0400

    se agrego la fecha en 20

--- 20(esta es por la de 16 registros).sql
+++ 20(esta es por la de 16 registros).sql
@@ -17,7 +17,8 @@ ON
 ied.desembolso = ief.id_desembolso and
 ied.monto_comision = ief.monto_facturado
 WHERE
-iep.metodo_credito = "Comision" #and
-#ied.tipo_comision = "Capitalizadas"
+iep.metodo_credito = "Comision" AND
+iep.fecha_transaccion >= "2018-01-01" AND
+iep.fecha_transaccion <= "2018-01-31"
 ORDER BY
 iep.nombre_cliente
