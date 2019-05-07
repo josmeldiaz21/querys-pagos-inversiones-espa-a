d3dea25465a20614d5e68efd8593c22358b35f9c
josmeldiaz21 <josmeldiaz21@gmail.com>
2019-05-07 12:23:53 -0400

    se añadio fecha en el primer query del union

--- UNION todos los queries..sql
+++ UNION todos los queries..sql
@@ -162,7 +162,7 @@ LEFT JOIN `tabJournal Entry` je
 ON
 iep.id_desembolso = je.id_de_desembolso
 WHERE
-	iep.fecha_transaccion BETWEEN "2018-01-01" AND "2018-01-31" AND
+	iep.fecha_transaccion BETWEEN %(from_date)s AND %(to_date)s AND
 	IF(iep.id_desembolso = je.id_de_desembolso,iep.metodo_credito = "Principal",iep.metodo_credito = "Comision") 
 ORDER BY
 nombre_cliente,
